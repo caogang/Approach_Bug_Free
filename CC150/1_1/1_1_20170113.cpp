ec6d3061ed8ee1fcd4a2244e8f324fb1b5e3a87b
MarvinCAO-MAC <caogang440@163.com>
2017-01-13 11:11:49 +0800

    CC1 BugFree

---
+++ CC150/1_1/1_1_20170113.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+bool isUniqueChars(string word) {
+  if (word.length() > 256) {
+    return false;
+  }
+
+  bool table[256];
+
+  for (int i = 0; i < word.length(); i++) {
+    if (table[word[i]]) {
+      return false;
+    }
+    table[word[i]] = true;
+  }
+
+  return true;
+}
+
+int main () {
+  string word;
+  cin >> word;
+  if (isUniqueChars(word)) {
+    cout << "Unique" << endl;
+  } else {
+    cout << "Not Unique" << endl;
+  }
+
+  return 0;
+}

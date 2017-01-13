60e598a48981aa116813f5615402d9efbee83c70
MarvinCAO-MAC <caogang440@163.com>
2017-01-13 10:01:17 +0800

    CC2

---
+++ CC150/1_2/1_2_20170113.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void reverse(char *str) {
+  char *end = str;
+  while (*end) {
+    end++;
+  }
+
+  end--;
+  char tp;
+
+  while (str < end) {
+    tp = *str;
+    *str++ = *end;
+    *end-- = tp;
+  }
+}
+
+int main() {
+  char str[5] = {'4', '3', '2', '1'};
+  reverse(str);
+  for (int i = 0; i < 4; i++) {
+    cout << str[i];
+  }
+  cout << endl;
+}
